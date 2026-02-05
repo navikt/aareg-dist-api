3ddcc02e7a8e9b90113d7a131e125f08dfcd3ef0
Knut Arild Slåtsve <knut.arild.slatsve@nav.no>
2026-02-05 12:53:22 +0100

    wip

--- src/main/java/no/nav/aareg/dist/api/config/RestClientConfig.java
+++ src/main/java/no/nav/aareg/dist/api/config/RestClientConfig.java
@@ -3,12 +3,15 @@ package no.nav.aareg.dist.api.config;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.aareg.dist.api.consumer.exception.RetryableException;
 import no.nav.aareg.dist.api.consumer.exception.UnrecoverableException;
+import org.apache.hc.client5.http.impl.DefaultHttpRequestRetryStrategy;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Slf4j
@@ -36,8 +39,9 @@ public class RestClientConfig {
         return restClientBuilder("Texas").build();
     }
 
-    public static RestClient.Builder restClientBuilder(String tjeneste) {
+    public RestClient.Builder restClientBuilder(String tjeneste) {
         return RestClient.builder()
+                .requestFactory(clientHttpRequestFactory())
                 .defaultStatusHandler(HttpStatusCode::is4xxClientError, (clientRequest, clientResponse) -> {
                     var melding = String.format("Tjeneste [%s] returnerte client error med status %s og melding %s", tjeneste, clientResponse.getStatusCode(), new String(clientResponse.getBody().readAllBytes()));
                     log.error(TEAM_LOGS_MARKER, melding);
@@ -48,5 +52,14 @@ public class RestClientConfig {
                     log.warn(TEAM_LOGS_MARKER, melding);
                     throw new RetryableException(melding);
                 });
+    }
+
+    @Bean
+    public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+        var client = HttpClients.custom()
+                .useSystemProperties()
+                .setRetryStrategy(new DefaultHttpRequestRetryStrategy())
+                .build();
+        return new HttpComponentsClientHttpRequestFactory(client);
     }
 }
